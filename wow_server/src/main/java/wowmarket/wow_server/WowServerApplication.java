1e1586b61a15e3183a6d2118d5f92240a64c782d
하윤지 <hayunji@hayunjiui-MacBookPro.local>
2023-07-18 15:10:34 +0900

    Z

---
+++ wow_server/src/main/java/wowmarket/wow_server/WowServerApplication.java
@@ -0,0 +1,13 @@
+package wowmarket.wow_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowServerApplication.class, args);
+	}
+
+}
---
+++ wow_server/src/test/java/wowmarket/wow_server/WowServerApplicationTests.java
@@ -0,0 +1,13 @@
+package wowmarket.wow_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

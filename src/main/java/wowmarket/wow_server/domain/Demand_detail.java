4adc488e0c4c3094ca70be0d914af93a9d4c59a4
하윤지 <hayunji@hayunjiui-MacBookPro.local>
2023-07-19 19:38:28 +0900

    [feat]demand entity 생성

---
+++ src/main/java/wowmarket/wow_server/domain/Demand_detail.java
@@ -0,0 +1,31 @@
+package wowmarket.wow_server.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Demand_detail extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "demand_detail_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "demand_item_id", referencedColumnName = "demand_item_id")
+    private Demand_item demand_item;
+
+//    @ManyToOne(fetch = FetchType.LAZY)이렇게 되면 수요조사 프로젝트 테이블과 연관관계 생기는것 아닌가?
+//    @JoinColumn(name = "demand_project_id", referencedColumnName = "demand_project_id")
+//    private Demand_project demand_project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id", referencedColumnName = "user_id")
+    private User user;
+    private Integer count;
+
+}
---
+++ src/main/java/wowmarket/wow_server/domain/Demand_project.java
@@ -0,0 +1,39 @@
+package wowmarket.wow_server.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+public class Demand_project extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "demand_project_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id", referencedColumnName = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+
+    private String name;
+    private String description;
+    private LocalDate start_date;
+    private LocalDate end_date;
+    //설명이미지필드
+    //대표이미지필드
+    //소개이미지1
+    //소개이미지2
+    //소개이미지3
+
+}

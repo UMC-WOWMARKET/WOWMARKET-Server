5416ac196b81d808357cb773737b998e99c95e7d
yunji118 <yunji118@naver.com>
2023-07-25 14:18:33 +0900

    [feat]user repo 생성 - 17

---
+++ src/main/java/wowmarket/wow_server/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package wowmarket.wow_server.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wowmarket.wow_server.domain.User;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+}

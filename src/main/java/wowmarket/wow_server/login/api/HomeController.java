24e16f9630c6a48939b00d20ba3ceb759e708d6a
yoonsseo <yellisa2000@gmail.com>
2023-07-26 17:30:11 +0900

    [Fix] 카카오 로그인 연결 끊기 구현 및 정보 제공 미동의 로직 수정

--- src/main/java/wowmarket/wow_server/login/api/HomeController.java
+++ src/main/java/wowmarket/wow_server/login/api/HomeController.java
@@ -22,35 +22,39 @@ public class HomeController {
 
     @RequestMapping(value="/wowmarket/users/login/kakao")
     public String login(@RequestParam("code") String code, HttpSession session) {
-        System.out.println("\n" + " =========================== ");
-        System.out.println("토큰 받기 요청에 필요한 인가 코드 : " + code +  "\n");
+        System.out.println("\n\n===============================================");
+        System.out.println("[login Controller] 토큰 받기 요청에 필요한 인가 코드 : " + code);
 
         String access_token = kakaoAPI.getAccessToken(code);
-        System.out.println("사용자 액세스 토큰 값 : " + access_token + "\n");
+        System.out.println("\n[login Controller] 사용자 액세스 토큰 값 : " + access_token);
+        session.setAttribute("access_token", access_token);
+        System.out.println("[login Controller] 사용자 액세스 토큰 값 세션에 저장 확인 session.getAttribute(\"access_token\") : " + session.getAttribute("access_token"));
 
         HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_token);
-        System.out.println("\n\n HashMap<String, Object>는 뭐가 나올까 userInfo = " + userInfo);
+        System.out.println("\n[login Controller] HashMap<String, Object> userInfo : " + userInfo);
+        System.out.println("[login Controller] userInfo.containsKey(\"email\") : " + userInfo.containsKey("email"));
         if (!userInfo.containsKey("email")) { //값이 없으면 false인데 !로 true로 바꿔서 이메일 없을 경우 로직 실행
-            System.out.println("\n\n 이메일 값이 넘어오지 않아서 다시 로그인 페이지 index() 호출 \n\n");
-            check_email(code);
+            System.out.println("[login Controller] 이메일 값이 넘어오지 않음!");
+            check_email(access_token);
+            System.out.println("\n[login Controller] check_email 호출");
         }
-        System.out.println("login Controller : " + userInfo);
+        System.out.println("[login Controller] login Controller userInfo : " + userInfo);
 
         //클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
         if (userInfo.get("email") != null) {
             session.setAttribute("userId", userInfo.get("email"));
-            System.out.println("세션 userId : " + session.getAttribute("userId"));
-            session.setAttribute("access_token", access_token);
+            System.out.println("[login Controller] 세션 userId 저장 : " + session.getAttribute("userId"));
+//            session.setAttribute("access_token", access_token);
         }
 
+        System.out.println("[login Controller] 로그인 성공!");
         return "index";
     }
 
     @PostMapping("/wowmarket/users/login/kakao/check_email")
     @ResponseBody // JSON 형태로 응답 반환을 위해 @ResponseBody 어노테이션 사용
-    public ResponseEntity<Map<String, String>> check_email(String code) {
-        System.out.println("\n\n 이메일 제공 미동의로 인한 로직 check_email");
-        String access_token = kakaoAPI.getAccessToken(code);
+    public ResponseEntity<Map<String, String>> check_email(String access_token) {
+        System.out.println("\n[check_email Controller] 이메일 제공 미동의로 인한 로직 check_email 실행");
 
         HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_token);
 
@@ -62,6 +66,16 @@ public class HomeController {
         }
 
         return ResponseEntity.ok(response);
+    }
+
+    @RequestMapping(value = "/wowmarket/users/kakao/unlink")
+    public String unlink(HttpSession session) {
+        System.out.println("\n[unlink Controller] 연결 끊기 로직 실행");
+        System.out.println("[unlink Controller] login Controller에서 세션에 access_token 넣었는데 토큰이 들어있는지 확인 session.getAttribute(\"access_token\") : " + session.getAttribute("access_token"));
+        kakaoAPI.unLink((String) session.getAttribute("access_token"));
+        session.invalidate();
+        System.out.println("\n[unlink Controller] 연결 끊기 성공!");
+        return "index";
     }
 
     @RequestMapping(value = "/logout")
--- src/main/java/wowmarket/wow_server/login/service/KakaoAPI.java
+++ src/main/java/wowmarket/wow_server/login/service/KakaoAPI.java
@@ -15,7 +15,9 @@ import wowmarket.wow_server.repository.UserRepository;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
+import java.util.Optional;
 
 /**
  * 인증 코드로 Access_Token을 받아올 작업을 수행할 Service 클래스 생성
@@ -48,7 +50,7 @@ public class KakaoAPI {
             bw.flush();
 
             int responseCode = conn.getResponseCode();
-            System.out.println("getAccessToken 상태코드 반환 : " + responseCode);
+            System.out.println("\n[getAccessToken] 상태코드 반환 : " + responseCode);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line = "";
@@ -56,7 +58,7 @@ public class KakaoAPI {
             while ((line = br.readLine()) != null) {
                 result += line;
             }
-            System.out.println("getAccessToken response body : " + result);
+            System.out.println("[getAccessToken] response body : " + result);
 
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
@@ -64,8 +66,8 @@ public class KakaoAPI {
             access_token = element.getAsJsonObject().get("access_token").getAsString();
             refresh_token = element.getAsJsonObject().get("refresh_token").getAsString();
 
-            System.out.println("access_token : " + access_token);
-            System.out.println("refresh_token : " + refresh_token);
+            System.out.println("[getAccessToken] access_token : " + access_token);
+            System.out.println("[getAccessToken] refresh_token : " + refresh_token);
 
             br.close();
             bw.close();
@@ -89,7 +91,7 @@ public class KakaoAPI {
             conn.setRequestProperty("Authorization", "Bearer " + access_token);
 
             int responseCode = conn.getResponseCode();
-            System.out.println("getUserInfo 상태코드 반환 : " + responseCode);
+            System.out.println("\n[getUserInfo] 상태코드 반환 : " + responseCode);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line = "";
@@ -97,38 +99,42 @@ public class KakaoAPI {
             while ((line = br.readLine()) != null) {
                 result += line;
             }
-            System.out.println("response body : " + result);
+            System.out.println("[getUserInfo] response body : " + result);
 
             JsonParser parser = new JsonParser();
             JsonElement element = parser.parse(result);
-            System.out.println(" \n\n element는 뭘까.. element = " + element);
+            System.out.println("[getUserInfo] element는 뭘까.. element = " + element);
 
             JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
             JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
 
-            System.out.println("\n JsonObject로 넘어오는 값은 뭘까 kakao_account = " + kakao_account);
-            System.out.println("\n 그러면 properties는 뭘까 properties = " + properties);
-            System.out.println(" \n 이메일 동의 했는지 확인 kakao_account.getAsJsonObject().get(\"email_needs_agreement\").getAsString() : " + kakao_account.getAsJsonObject().get("email_needs_agreement").getAsString() + "\n\n");
+            System.out.println("[getUserInfo] JsonObject kakao_account로 넘어오는 값은 뭘까 kakao_account = " + kakao_account);
+            System.out.println("[getUserInfo] 그러면 JsonObject properties는 뭘까 properties = " + properties);
+            System.out.println("[getUserInfo] 이메일 동의 필요한지 확인 kakao_account.getAsJsonObject().get(\"email_needs_agreement\").getAsString() : " + kakao_account.getAsJsonObject().get("email_needs_agreement").getAsString());
 
             String nickname = properties.getAsJsonObject().get("nickname").getAsString();
             //email_needs_agreement로 이메일 동의 했는지 확인
             //동의 안 했으면 빈 해쉬값이 넘어가서 controller에서 연결 끊기 api가 실행되고 다시 로그인 하도록 설정
             //email_needs_agreement 가 true 이면 이메일 정보 제공에 동의를 안 해서 동의가 필요하다는 뜻
             if (kakao_account.getAsJsonObject().get("email_needs_agreement").getAsString() == "true") {
+                System.out.println("[getUserInfo] 이메일 제공 미동의로 [getUserInfo] 종료");
                 return userInfo;
             }
             String email = kakao_account.getAsJsonObject().get("email").getAsString();
 
-            System.out.println("getUserInfo nickname = " + nickname);
-            System.out.println("getUserInfo email = " + email);
+            System.out.println("[getUserInfo] nickname = " + nickname);
+            System.out.println("[getUserInfo] email = " + email);
 //
 //            System.out.println("\n\n **** userRepository.findByEmail(email).isEmpty() : " + userRepository.findByEmail(email).isEmpty());
 //            System.out.println("userRepository.findByEmail(email) : " + userRepository.findByEmail(email) + "\n\n");
-
-            //findByEmail로 값이 없으면 DB에 저장
-            if (userRepository.findByEmail(email).isEmpty()) {
+            Optional<User> userFindbyEmail = userRepository.findByEmail(email);
+            //findByEmail로 값이 없으면 DB에 저장 userRepository.findByEmail(email).isEmpty()
+            if (userFindbyEmail.isEmpty()) { // 쿼리 너무 많이 나가서 -> 추후에 userRepository.findByEmail(email).isEmpty() 로 교체
+                System.out.println("\n[getUserInfo] userId와 email 값 잘 넘어왔고 DB에 해당 email 없으면 DB에 저장하는 로직 실행");
+                System.out.println("[getUserInfo] DB 저장 전 Optional<User> userFindbyEmail = userRepository.findByEmail(email) : " + userFindbyEmail);
                 KakaoDto kakaoDto = new KakaoDto(email, nickname, Login_Method.KAKAO);
                 User User = userRepository.save(kakaoDto.toEntity());
+                System.out.println("[getUserInfo] DB 저장 후 User 테이블 userId userRepository.findByEmail(email).get().getId() : " +  userRepository.findByEmail(email).get().getId());
             }
 
             userInfo.put("nickname", nickname);
@@ -139,6 +145,32 @@ public class KakaoAPI {
         }
 
         return userInfo;
+    }
+
+    public void unLink(String access_token) {
+        String reqURL = "https://kapi.kakao.com/v1/user/unlink";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Bearer " + access_token);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("\n[unLink] 상태코드 반환 : " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("[unLink] response body : " + result);
+            //해당 id는 연결 끊기에 성공한 사용자의 회원번호
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void kakaoLogout(String acccess_token) {

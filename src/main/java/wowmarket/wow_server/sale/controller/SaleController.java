6ae2f11756063c867b06edfb297ded637ab31cb2
yoonsseo <yellisa2000@gmail.com>
2023-08-04 14:25:29 +0900

    커밋 메시지를 입력하세요

--- src/main/java/wowmarket/wow_server/sale/controller/SaleController.java
+++ src/main/java/wowmarket/wow_server/sale/controller/SaleController.java
@@ -20,13 +20,12 @@ public class SaleController {
     public SaleResponseDto GetSaleProjectListHome(
             @RequestParam(name = "univ", defaultValue = "allUniv", required = true) String univ, //myUniv, allUniv
             @RequestParam(name = "orderby", defaultValue = "endDate", required = true) String orderby, //endDate, popularity, createdDate
-            @RequestParam("lastpostid") Long lastpostid, //무한스크롤 구현을 위해 현재 보고 있는 상품의 마지막 id
-            @RequestParam("size") int size) { //상품 몇 개씩 보여줄지 size
-        //로그인이 되지 않은 상태면 univ = all
-
+            @RequestParam(name = "lastpostid") Long lastpostid, //무한스크롤 구현을 위해 현재 보고 있는 상품의 마지막 id
+            @RequestParam(name = "size", defaultValue = "9", required = true) int size) { //상품 몇 개씩 보여줄지 size
         System.out.println("\n[GetSaleProjectListHome Controller] 판매 홈 페이지 로직\n");
-        return saleHomeService.findProjectHome(univ, orderby);
+        return saleHomeService.findProjectHome(univ, orderby, lastpostid, size);
     }
+
 
     @GetMapping
 //     /wowmarket/sale?search={search}&univ=${univ}&orderby=${orderby}&lastpostid=${lastpostid}&size=${size}
@@ -35,7 +34,7 @@ public class SaleController {
             @RequestParam(name = "univ", defaultValue = "allUniv", required = true) String univ, //myUniv, allUniv
             @RequestParam(name = "orderby", defaultValue = "endDate", required = true) String orderby, //endDate, popularity, createdDate
             @RequestParam("lastpostid") Long lastpostid, //무한스크롤 구현을 위해 현재 보고 있는 상품의 마지막 id
-            @RequestParam("size") int size) {
+            @RequestParam(name = "size", defaultValue = "9", required = true) int size) {
         System.out.println("\n[GetSaleProjectListSearch Controller] 판매 검색 페이지 로직\n");
         return saleSearchService.findProjectSearch(search, univ, orderby);//로그인 유무와 유저 정보 필요!!
     }

307bd8809c8dab5a0c8aadaf4c1df3fa1f59b672
Minji Kim <minji1289@naver.com>
2023-08-05 22:46:23 +0900

    [feat] 공지 작성 및 조회 기능 구현(유저 권한 반영 x)

--- src/main/java/wowmarket/wow_server/domain/Notice.java
+++ src/main/java/wowmarket/wow_server/domain/Notice.java
@@ -2,14 +2,39 @@ package wowmarket.wow_server.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import wowmarket.wow_server.detail.notice.dto.NoticeRequestDto;
 
 @Entity
 @Getter
-public class Notice {
+@Table(name = "notice")
+@NoArgsConstructor
+public class Notice extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "notice_id")
     private Long id;
 
+    //User 테이블 관련 로직
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @Column(name = "notice_title", nullable=false)
     private String title;
+
+    @Column(name = "notice_content", nullable=false, length=500)
+    private String content;
+
+    // 게시글 작성
+    public Notice(NoticeRequestDto requestDto) {
+        this.title = requestDto.getTitle();
+        this.content = requestDto.getContent();
+    }
+
+//    // 게시글 수정
+//    public void update(NoticeRequestDto requestDto) {
+//        this.title = requestDto.getTitle();
+//        this.content = requestDto.getContent();
+//    }
 }

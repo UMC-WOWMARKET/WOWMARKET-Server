e00341766ee6b08af59cf9f263e647f7ed8dfa17
‘yj-leez’ <yj_leez@naver.com>
2023-08-07 17:33:22 +0900

    [feat] s3 이미지 업로드 presigned url 리턴으로 수정

--- src/main/java/wowmarket/wow_server/global/S3Uploader.java
+++ src/main/java/wowmarket/wow_server/global/S3Uploader.java
@@ -1,7 +1,11 @@
 package wowmarket.wow_server.global;
 
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AbstractAmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.Headers;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,14 +13,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import wowmarket.wow_server.global.config.S3Config;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.net.URL;
+import java.util.*;
 
 
 @Slf4j
@@ -29,6 +32,37 @@ public class S3Uploader {
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+
+    public String getPreSignedURL(String dirName) {
+        String preSignedURL = "";
+        String fileName = dirName + "/" + UUID.randomUUID().toString();
+
+        Date expiration = new Date();
+        long expTimeMillis = expiration.getTime();
+        expTimeMillis += 1000 * 60 * 2; //2분
+        expiration.setTime(expTimeMillis);
+
+
+        try {
+            GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                    new GeneratePresignedUrlRequest(bucket, fileName)
+                            .withMethod(HttpMethod.PUT)
+                            .withExpiration(expiration);
+
+            generatePresignedUrlRequest.addRequestParameter(
+                    Headers.S3_CANNED_ACL,
+                    CannedAccessControlList.PublicRead.toString());
+
+            URL url = amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
+            preSignedURL = url.toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return preSignedURL;
+    }
+
 
     // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
     public List<String> upload(List<MultipartFile> multipartFileList, String dirName) throws IOException {
--- src/main/java/wowmarket/wow_server/register/controller/RegisterController.java
+++ src/main/java/wowmarket/wow_server/register/controller/RegisterController.java
@@ -3,6 +3,7 @@ package wowmarket.wow_server.register.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import wowmarket.wow_server.domain.Category;
@@ -24,11 +25,26 @@ public class RegisterController {
 
     @PostMapping("/project")
     @ResponseStatus(HttpStatus.OK)
-    public Long registerProject(@Valid @RequestPart(value = "dto") RegisterProjectDto request,
-                   @RequestPart(value = "files") List<MultipartFile> files) throws Exception {
-        List<String> uploaded = awsS3Uploader.upload(files, "project");
-        Long project_id = registerService.registerProject(request, uploaded);
+    public Long registerProject(@Valid @RequestBody RegisterProjectDto request) throws Exception {
+        Long project_id = registerService.registerProject(request);
         return project_id;
+    }
+
+    @PostMapping("/demand")
+    @ResponseStatus(HttpStatus.OK)
+    public Long registerDemand(@Valid @RequestBody RegisterDemandProjectDto request) throws Exception {
+        return registerService.registerDemand(request);
+    }
+
+    /**
+     * client에 presinged url 반환하여 이미지 직접 put 가능
+     * @param dirname 버킷 내 폴더이름 - project, demand
+     * @return presigned url
+     */
+    @GetMapping("/image")
+    @ResponseStatus(HttpStatus.OK)
+    public String RegisterProject(String dirname){
+        return awsS3Uploader.getPreSignedURL(dirname);
     }
 
     @GetMapping(value = {"/project", "/demand"})
@@ -37,20 +53,26 @@ public class RegisterController {
         return registerService.findCategories();
     }
 
-//    @PostMapping("/demand")
+
+
+
+//    @PostMapping("/project")
 //    @ResponseStatus(HttpStatus.OK)
-//    public Long registerDemand(@Valid @RequestBody RegisterDemandProjectDto request) throws Exception {
-//        return registerService.registerDemand(request);
+//    public Long registerProject(@Valid @RequestPart(value = "dto") RegisterProjectDto request,
+//                   @RequestPart(value = "files") List<MultipartFile> files) throws Exception {
+//        List<String> uploaded = awsS3Uploader.upload(files, "project");
+//        Long project_id = registerService.registerProject(request, uploaded);
+//        return project_id;
 //    }
 
-    @PostMapping("/demand")
-    @ResponseStatus(HttpStatus.OK)
-    public Long registerDemand(@Valid @RequestPart(value = "dto") RegisterDemandProjectDto request,
-                                @RequestPart(value = "files") List<MultipartFile> files) throws Exception {
-        List<String> uploaded = awsS3Uploader.upload(files, "demand");
-        Long demand_project_id = registerService.registerDemand(request, uploaded);
-        return demand_project_id;
-    }
+//    @PostMapping("/demand")
+//    @ResponseStatus(HttpStatus.OK)
+//    public Long registerDemand(@Valid @RequestPart(value = "dto") RegisterDemandProjectDto request,
+//                                @RequestPart(value = "files") List<MultipartFile> files) throws Exception {
+//        List<String> uploaded = awsS3Uploader.upload(files, "demand");
+//        Long demand_project_id = registerService.registerDemand(request, uploaded);
+//        return demand_project_id;
+//    }
 
 
 

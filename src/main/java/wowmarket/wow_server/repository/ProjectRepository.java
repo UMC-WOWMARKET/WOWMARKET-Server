391d8bde7d33b0335a1cd63453381449b1ba22c4
yoonsseo <yellisa2000@gmail.com>
2023-08-08 16:37:42 +0900

    [Fix] 프로젝트 종료여부 삭제여부 확인

--- src/main/java/wowmarket/wow_server/repository/ProjectRepository.java
+++ src/main/java/wowmarket/wow_server/repository/ProjectRepository.java
@@ -10,10 +10,27 @@ import wowmarket.wow_server.domain.Project;
 import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    List<Project> findByNameContaining(String name);
-
-    @Query("SELECT p FROM Project p WHERE p.user.univ = :user_univ")
+    @Query("SELECT p FROM Project p " +
+            "WHERE p.user.univ = :user_univ " +
+            "AND p.isDel = false " +
+            "AND p.isEnd = false")
     List<Project> findProjectByUserUniv(@Param("user_univ") String user_univ);
+
+    @Query("SELECT p FROM Project p WHERE p.isDel = false AND p.isEnd = false")
+    List<Project> findAllProjectNotDelNotEnd();
+
+    @Query("SELECT p FROM Project p " +
+            "WHERE p.user.univ = :user_univ " +
+            "AND p.name LIKE CONCAT('%', :search, '%') " +
+            "AND p.isDel = false " +
+            "AND p.isEnd = false")
+    List<Project> findProjectByUserUnivAndSearch(@Param("user_univ") String user_univ, @Param("search") String search);
+
+    @Query("SELECT p FROM Project p " +
+            "WHERE p.name LIKE CONCAT('%', :search, '%') " +
+            "AND p.isDel = false " +
+            "AND p.isEnd = false")
+    List<Project> findProjectBySearch(@Param("search") String search);
 
     Page<Project> findByUser_Id(Long sellerId, Pageable pageable);
 

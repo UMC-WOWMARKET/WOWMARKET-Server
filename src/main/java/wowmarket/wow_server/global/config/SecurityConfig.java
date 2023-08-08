0a9e1d3f1da4c12508e6bf7b759a656807157d20
‘yj-leez’ <yj_leez@naver.com>
2023-08-08 17:49:26 +0900

    [fix] aws 오류 해결

--- src/main/java/wowmarket/wow_server/global/config/SecurityConfig.java
+++ src/main/java/wowmarket/wow_server/global/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package wowmarket.wow_server.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -20,7 +20,7 @@ import wowmarket.wow_server.global.jwt.JwtTokenProvider;
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
-    private final RedisTemplate redisTemplate;
+//    private final RedisTemplate redisTemplate;
 
     @Bean //비밀번호 암호화 처리하는 메서드 제공
     public PasswordEncoder passwordEncoder() {
@@ -41,7 +41,7 @@ public class SecurityConfig {
                 .httpBasic(httpBasic -> httpBasic.disable()); // http 기본 인증 비활성화
 
 
-        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisTemplate),
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                 UsernamePasswordAuthenticationFilter.class);
 
         // 세션을 사용하지 않기 때문에 STATELESS로 설정

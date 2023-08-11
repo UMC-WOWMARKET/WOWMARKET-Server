f650c7e1709c923616c2f74098cd24fdea48d0dc
‘yj-leez’ <yj_leez@naver.com>
2023-08-12 00:02:20 +0900

    [fix] cors 에러 해결 시도

--- src/main/java/wowmarket/wow_server/global/config/WebMvcConfig.java
+++ src/main/java/wowmarket/wow_server/global/config/WebMvcConfig.java
@@ -7,25 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 이 설정 없으면 모든 origin에 대해 허용
-                .allowedOrigins("http://13.125.190.15:8080")
-//                .allowedMethods("*") // 기타 설정
-//                .allowedHeaders("*");
-                .allowedMethods(ALLOWED_METHOD_NAMES.split(",")) //허용할 메소드
-                .exposedHeaders(HttpHeaders.LOCATION); // 서버에서 반환해 줄 헤더
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS","TRACE") //허용할 메소드
+                .allowCredentials(true)
+                .maxAge(3600);
     }
+//@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 
-//    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
-//
-//    @Override
-//    public void addCorsMappings(final CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedMethods(ALLOWED_METHOD_NAMES.split(",")) //허용할 메소드
-//                .exposedHeaders(HttpHeaders.LOCATION); // 서버에서 반환해 줄 헤더
-//    }
 }

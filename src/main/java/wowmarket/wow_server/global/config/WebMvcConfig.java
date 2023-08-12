48779674c7cffc047b7c22ed7a90443c81a98b79
yj-leez <77960090+yj-leez@users.noreply.github.com>
2023-08-12 16:01:35 +0900

    Update WebMvcConfig.java

--- src/main/java/wowmarket/wow_server/global/config/WebMvcConfig.java
+++ src/main/java/wowmarket/wow_server/global/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override

f5b73aa9db56db11fd24e79f5042f59c9b792287
yj-leez <77960090+yj-leez@users.noreply.github.com>
2023-08-12 16:03:16 +0900

    Update WebMvcConfig.java

--- src/main/java/wowmarket/wow_server/global/config/WebMvcConfig.java
+++ src/main/java/wowmarket/wow_server/global/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import org.apache.http.HttpHeaders;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {

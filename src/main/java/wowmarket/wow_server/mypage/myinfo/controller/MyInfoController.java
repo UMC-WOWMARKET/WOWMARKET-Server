0a9beafca081ede27020a33f34ca1d87b985cfbc
yunji118 <yunji118@naver.com>
2023-08-15 16:40:38 +0900

    [feat]myinfo url 변경

--- src/main/java/wowmarket/wow_server/mypage/myinfo/controller/MyInfoController.java
+++ src/main/java/wowmarket/wow_server/mypage/myinfo/controller/MyInfoController.java
@@ -12,7 +12,7 @@ import wowmarket.wow_server.mypage.myinfo.dto.MyInfoResponseDto;
 import wowmarket.wow_server.mypage.myinfo.service.MyInfoService;
 
 @RestController
-@RequestMapping("/mypage")
+@RequestMapping("/mypage/myinfo")
 @RequiredArgsConstructor
 public class MyInfoController {
     private final MyInfoService myInfoService;

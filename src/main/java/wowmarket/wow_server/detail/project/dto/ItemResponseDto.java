26c68c006122f5721483d1342e25cd97454b3430
Minji Kim <minji1289@naver.com>
2023-08-15 20:07:26 +0900

    [fix] 주문폼 로드시 item_id도 함께 불러오기

--- src/main/java/wowmarket/wow_server/detail/project/dto/ItemResponseDto.java
+++ src/main/java/wowmarket/wow_server/detail/project/dto/ItemResponseDto.java
@@ -7,11 +7,12 @@ import wowmarket.wow_server.domain.Item;
 
 @Getter
 public class ItemResponseDto {
-
+    private Long id; //item_id
     private String name; //상품명
     private Long price; //판매가
 
     public ItemResponseDto(Item item) {
+        this.id = item.getId(); //item_id 추가
         this.name = item.getName(); //상품명
         this.price = item.getPrice(); //판매가
    }

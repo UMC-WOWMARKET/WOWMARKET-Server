5652a48ea43dc4f2d068dcfeadc4c41501dd933f
yunji118 <yunji118@naver.com>
2024-01-04 20:03:41 +0900

    [feat]판매 주문폼 상세보기 dto 수정 #136

--- src/main/java/wowmarket/wow_server/mypage/myorder/sales/dto/MyOrderFormDetailResponseDto.java
+++ src/main/java/wowmarket/wow_server/mypage/myorder/sales/dto/MyOrderFormDetailResponseDto.java
@@ -10,60 +10,60 @@ import java.util.List;
 @NoArgsConstructor
 public class MyOrderFormDetailResponseDto {
 
-    private Long order_id;
-    private String project_name;
+    private Long orderId;
+    private String projectName;
     private String description;
-    private int total_price;
+    private int totalPrice;
     private String thumbnail;
     private int status;
-    private int is_del;
+    private int isDel;
 
     private List<MyOrderFormDetailDto> itemList;
     private String receiver;
     private String address;
-    private Long delivery_fee;
+    private Long deliveryFee;
     private String zipcode;
     private String message;
     private String phone;
 
-    private String seller_bank;
-    private String seller_account;
-    private String seller_account_name;
+    private String sellerBank;
+    private String sellerAccount;
+    private String sellerAccountName;
 
-    private String deposit_name;
-    private String deposit_time;
+    private String depositName;
+    private String depositTime;
 
-    private String refund_bank;
-    private String refund_account;
+    private String refundBank;
+    private String refundAccount;
 
 
     public MyOrderFormDetailResponseDto(List<MyOrderFormDetailDto> itemList, Orders orders, String address){
-        this.order_id = orders.getId();
-        this.project_name = orders.getProject().getProjectName();
+        this.orderId = orders.getId();
+        this.projectName = orders.getProject().getProjectName();
         this.description = orders.getProject().getDescription();
-        this.total_price = orders.getTotal_price();
+        this.totalPrice = orders.getTotal_price();
         this.thumbnail = orders.getProject().getThumbnail();
         this.status = orders.getOrder_status();
-        this.is_del = orders.getIsDel();
+        this.isDel = orders.getIsDel();
 
         this.itemList = itemList;
 
         this.receiver = orders.getReceiver();
         this.address = address;
-        this.delivery_fee = orders.getProject().getDelivery_fee();
+        this.deliveryFee = orders.getProject().getDelivery_fee();
         this.zipcode = orders.getZipcode();
         this.message = orders.getDelivery_msg();
         this.phone = orders.getPhone();
 
-        this.seller_bank = orders.getProject().getBank();
-        this.seller_account = orders.getProject().getAccount();
-        this.seller_account_name = orders.getProject().getAccount_holder_name();
+        this.sellerBank = orders.getProject().getBank();
+        this.sellerAccount = orders.getProject().getAccount();
+        this.sellerAccountName = orders.getProject().getAccount_holder_name();
 
-        this.deposit_name = orders.getDepositor();
-        this.deposit_time = orders.getDepositTime();
+        this.depositName = orders.getDepositor();
+        this.depositTime = orders.getDepositTime();
 
-        this.refund_bank = orders.getBank();
-        this.refund_account = orders.getAccount();
+        this.refundBank = orders.getBank();
+        this.refundAccount = orders.getAccount();
     }
 
 }

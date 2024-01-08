aa050b70860ab1a1857453837d9220f4159360a7
yunji118 <yunji118@naver.com>
2024-01-08 22:47:43 +0900

    [feat] 관리자로 권한 변경 #154

---
+++ src/main/java/wowmarket/wow_server/admin/adminAccount/controller/AdminAccountManagementController.java
@@ -0,0 +1,27 @@
+package wowmarket.wow_server.admin.adminAccount.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wowmarket.wow_server.admin.adminAccount.dto.ChangeRoleRequestDto;
+import wowmarket.wow_server.admin.adminAccount.service.AdminAccountManagementService;
+import wowmarket.wow_server.domain.User;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminAccountManagementController {
+    private final AdminAccountManagementService adminAccountManagementService;
+
+    @PutMapping("/role/admin")
+    public ResponseEntity giveAdminRole(@RequestBody ChangeRoleRequestDto requestDto, @AuthenticationPrincipal User user){
+        return adminAccountManagementService.giveAdminRole(requestDto, user);
+    }
+
+
+
+}
---
+++ src/main/java/wowmarket/wow_server/admin/adminAccount/dto/ChangeRoleRequestDto.java
@@ -0,0 +1,8 @@
+package wowmarket.wow_server.admin.adminAccount.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ChangeRoleRequestDto {
+    private String email;
+}
